51e4b4d3770c4ce09326dc6a5d4f1036ba4f1344
aquma25 <h.aquma25@gmail.com>
2020-08-27 17:30:10 +0900

    BugFix 不必要な記述の削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_26_140741) do
+ActiveRecord::Schema.define(version: 2020_08_26_102209) do
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "provider"
@@ -32,7 +32,6 @@ ActiveRecord::Schema.define(version: 2020_08_26_140741) do
     t.string "unconfirmed_email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "nick_name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
